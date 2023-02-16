fa21a7e8272a5f5f9c083122d85775e7eeae846c
unknown <amarjeet@baruzotech.com>
2023-02-16 10:30:32 +0530

    code complete waiting for review

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
 import express from 'express';
 import sgMail from '@sendgrid/mail';
+import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const msg = {
-  to: 'amajeetdev72@gmail.com',
-  from: 'amarjeet@baruzotech.com', // Use the email address or domain you verified above
+  to: ['ravi@baruzotech.com', 'sarbhadi@gmail.com'],
+  from: { name: 'amarjeet testing', email: 'amarjeet@baruzotech.com' }, // Use the email address or domain you verified above
   subject: 'Sending with Twilio SendGrid is Fun',
   text: 'and easy to do anywhere, even with Node.js',
   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
 };
 app.post('/mail', async (req, res) => {
-  sgMail
-    .send(msg)
-    .then((response) => {
-      console.log('email sent');
-    })
-    .catch((error) => {
-      console.log('error===>>', error);
-    });
+  sgMail.send(msg).then(
+    (response) => {
+      console.log('response====>>', response);
+    },
+    (error) => {
+      console.error(error);
+      if (error.response) {
+        console.error(error.response.body);
+      }
+    }
+  );
 });
 
 const PORT = process.env.PORT || 3000;
