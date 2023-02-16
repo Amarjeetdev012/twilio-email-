3e9a6b1e30a72f6d12701a332307377c8f24cec5
unknown <amarjeet@baruzotech.com>
2023-02-16 11:55:35 +0530

    mail send done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import sgMail from '@sendgrid/mail';
+import nodemailer from 'nodemailer';
 import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -7,26 +7,39 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const msg = {
-  to: ['ravi@baruzotech.com', 'sarbhadi@gmail.com'],
-  from: { name: 'amarjeet testing', email: 'amarjeet@baruzotech.com' }, // Use the email address or domain you verified above
-  subject: 'Sending with Twilio SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-};
-app.post('/mail', async (req, res) => {
-  sgMail.send(msg).then(
-    (response) => {
-      console.log('response====>>', response);
-    },
-    (error) => {
-      console.error(error);
-      if (error.response) {
-        console.error(error.response.body);
-      }
-    }
-  );
+
+app.post('/email', async (req, res) => {
+  try {
+    let testAccount = await nodemailer.createTestAccount();
+    console.log('testAccount', testAccount);
+    // create reusable transporter object using the default SMTP transport
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      auth: {
+        user: process.env.SMTP_USERNAME,
+        pass: process.env.SMTP_PASSWORD,
+      },
+    });
+    console.log('transporter', transporter);
+    // send mail with defined transport object
+    const info = await transporter.sendMail({
+      from: process.env.SMTP_FROM,
+      to: 'sarbhadi@gmail.com, ravi@baruzotech.com',
+      subject: 'Hello ✔', // Subject line
+      text: 'Hello world?', // plain text body
+      html: '<b>Hello world?</b>', // html body
+    });
+
+    console.log('Message sent: %s', info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  } catch (error) {
+    console.log('error==', error);
+  }
 });
 
 const PORT = process.env.PORT || 3000;
