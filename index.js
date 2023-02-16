a035db7d0cc9592af7972f4d7debc587cdd89815
unknown <amarjeet@baruzotech.com>
2023-02-16 09:59:40 +0530

    email sent

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 import express from 'express';
+import sgMail from '@sendgrid/mail';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
+app.use(express.json());
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const msg = {
+  to: 'amajeetdev72@gmail.com',
+  from: 'amarjeet@baruzotech.com', // Use the email address or domain you verified above
+  subject: 'Sending with Twilio SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
+app.post('/mail', async (req, res) => {
+  sgMail
+    .send(msg)
+    .then((response) => {
+      console.log('email sent');
+    })
+    .catch((error) => {
+      console.log('error===>>', error);
+    });
+});
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`app is runnning on PORT ${PORT}`);
 });
